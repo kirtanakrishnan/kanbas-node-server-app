2910ea682df1c70eac3c2ac9e4925ae59b51cf93
kirtanakrishnan <kirtana.k8@gmail.com>
2023-11-17 09:05:13 -0500

    cors changes

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ import "dotenv/config";
 const app = express()
 app.use(cors());
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
+    cors()
   );  
 app.use(express.json());
 CourseRoutes(app);
