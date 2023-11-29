75de888c770d74fed7e3dab7cb2a974a066f28a4
kirtanakrishnan <kirtana.k8@gmail.com>
2023-11-28 20:23:35 -0500

    add Lab5

--- Lab5.js
+++ Lab5.js
@@ -15,20 +15,6 @@ const assignment = {
   
   
   function Lab5(app) {
-
-   
-
-    app.get("/a5/todos/create", (req, res) => {
-      const newTodo = {
-        id: new Date().getTime(),
-        title: "New Task",
-        completed: false,
-      };
-      todos.push(newTodo);
-      res.json(todos);
-    });
-    
-
     app.post("/a5/todos", (req, res) => {
         const newTodo = {
           ...req.body,
